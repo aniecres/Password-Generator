06d53021688a6e704c95914f556f7826a178a475
aniecres <111096788+aniecres@users.noreply.github.com>
2022-10-30 14:23:15 -0400

    update js

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,28 @@
-// Variables that will be included in produced pasword 
+// Characters/Numbers that will be included in produced pasword 
 character = ["!", "@", "#", "$", "%", "^", "&"];
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// upperCase choice
+var upperCase = function () {
+    return () to upperCase();
+};
+
+let upperCase 
+
+// generating the password
+function generate() {
+    enter = parseint(prompt(""))
+}
+
+// any letter maps to the uppercase function when called
+alphabetUp = alphabet.map(upperCase);
+ get = document.querySelector(#"generate");
+
+ get.addEventListener("click", function()) {
+    newPassword = generatePassword();
+    document.getElementById("password").ariaPlaceholder = newPassword;
+ };
+
+// generating the password
 
