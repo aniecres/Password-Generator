0f27bcf44cb5f67c1496405b9796b8b3bce67e43
aniecres <111096788+aniecres@users.noreply.github.com>
2022-10-30 13:29:57 -0400

    start js

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+// Variables that will be included in produced pasword 
+character = ["!", "@", "#", "$", "%", "^", "&"];
+numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
